4edbe2b41342afdffdc55ad02ff0f9dfea94f89b
efimi <efimi@me.com>
2018-01-15 14:06:13 +0100

    changed allert

--- resources/views/alert.blade.php
+++ resources/views/alert.blade.php
@@ -9,22 +9,15 @@
         <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     </head>
     <body>
-        <!-- <p>{!! asset('css/sweetalert.css')!!}</p> -->
-    <!-- Get ready to output some awesome notifications! -->
-        <!-- @if (notify()->ready())
-            <div class="alert alert-{{ notify()->type() }}">
-                {{ notify()->message() }}
-            </div>
-        @endif -->
-    
-        <!-- <script src="{{ asset('js/sweetalert.min.js')}}"></script> -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
-
+       
+        <script src="{{ asset('js/sweetalert.min.js')}}"></script>
+         @if (notify()->ready())
         <script>
             swal({
-                title: 'Great job',
-                type: 'success'
+                title: "{{ notify()->message()}}",
+                type: "{{ notify()->type()}}",
             });
         </script>
+        @endif
     </body>
 </html>

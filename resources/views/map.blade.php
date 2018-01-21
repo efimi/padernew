5397a4b26bc7aacf124c1df9618cae31e556f689
efimi <efimi@me.com>
2018-01-21 15:18:35 +0100

    map kleiner gemacht

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,7 +1,7 @@
 <div>
 	 <iframe
-      width="600"
-      height="450"
+      width="300"
+      height="200"
       frameborder="0" style="border:0"
       src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAqgEFYIDmSRJL9ytPuyjs2tVqHtiu61zM
         &q={{!! $location->address !!}},Paderborn" allowfullscreen>

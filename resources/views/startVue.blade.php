1b2a810b81823d0438c3b3b6b38bf1640747c024
efimi <efimi@me.com>
2018-01-29 19:37:31 +0100

    commit before cleaingn up

--- resources/views/startVue.blade.php
+++ resources/views/startVue.blade.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <title>PaderMeet</title>
-    {{-- <link rel="stylesheet" href="https://unpkg.com/blaze"> --}}
     {{-- Only for locations --}}
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <style>
@@ -11,27 +10,108 @@
         background-color: #edeff0;
         color:#333;
         font-size:2em;
-
+    }
+    .logo{
+        width: auto;
+        display: block;
+        margin-top: 1em;
+        z-index: 0;
+    }
+    .modal{
+        position: fixed;
+        top: 0; left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, .8);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        pointer-events: none;
+    }
+    .modal__container{
+        border-radius: 50px;
+        background-color: #fff;
+        padding: 40px;
+        max-width: 400px;
+        margin: 20px;
+        pointer-events: all;
+    }
+    @media (max-width: 768px) {
+        .modal {
+            align-items: flex-start;
+        }
     }
     </style>
 </head>
 <body>
     <script src="https://unpkg.com/vue@2.5.13/dist/vue.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/locale/de.js"></script>
-        <div id="app" :style="style.appStyle">
-            {{-- tutorial-swipe --}}
+    
+    <div id="app">
+        <div class="navigation">
+            <button href="#" class="login-button" @click="showLoginModal = true">Login</button>
+        </div>
+
+        <div class="logo">
+            <img src="img/logo.png" alt="">
+        </div>
+
+        <div class="start-button">
+            <span>Finde deine Location!</span>
+        </div>
+
+        <div class="result">
+            <div class="result__content">
+                <div class="location">
+                    <h1>Klara</h1>
+                    <div class="location__map">
+                        <iframe
+                          width="300"
+                          height="200"
+                          frameborder="0" style="border:0"
+                          src="https://maps.google.com/?cid=5427443445385247679" allowfullscreen>
+                        </iframe>
+                    </div>
+                    <div class="location__photos">
+                        <div class="swipe-carousell">
+                            <img src="https://lh3.googleusercontent.com/p/AF1QipOdqX2fI-6geEFXjvivVzuuji4OHWawAEpSIlZy=s1600-w500-h500" alt="">
+                            <img src="https://lh3.googleusercontent.com/p/AF1QipOZa9cjuV7wmHQp7Os0zwpm2bAr-w-UY1L7bU9t=s1600-w500-h500" alt="">
+                            <img src="https://lh3.googleusercontent.com/p/AF1QipPYS1losQiYuNQDUmcBt7vNnUGi7poUvqBHfVru=s1600-w500-h500" alt="">
+                        </div>
+                    </div>
+                    <div class="location__info">
+                        <a href="https://www.facebook.com/karlamachtmichan">Link zur Location</a>
+                    </div>
+                </div>
+            </div>
+        </div>    
+
+        <div class="modal" :show="showLoginModal">
+            <div class="modal__container">
+                <div class="login-modal" style="display: flex; flex-direction: column; align-items: center; max-width: inherit;">
+                    <h2>Lege Los!</h2>
+                    <div class="logo">
+                        <img src="img/logo-small.png"  style="transform: scale(0.5, 0.5);;" alt="">
+                    </div>
+                    <h3>Logge dich ein:</h3>
+                    
+                   <div class="button-group" style="display: flex; flex-direction: column;" >
+                        <button class="facebook-login" style="width: 300px; margin-top: 1em; border-radius: 100px; background-color: skyblue; padding: 1em 2em; font-size: 16px;">Mit Facebook</button>
+                        <button class="email-toggle" style="width: 300px; margin-top: 1em; border-radius: 100px; background-color: skyblue; padding: 1em 2em; font-size: 16px;">mit Email</button>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </div><-- #app -->
+      
+
+        {{-- <div id="app" :style="style.appStyle">
             <navigation></navigation>
             <logo path="img/logo.png"></logo>
             <start-button></start-button>
             <result-location></result-location>
-            {{-- <h1> Willkommen bei PaderMeet</h1>
-            <h2> Drücke auf den Button und finde heraus wo es für dich hingeht!</h2>
-            <invite-text></invite-text>
-            <count-down-today time="16" v-show=></count-down-today>
-            <location-box></location-box> --}}
-        </div>
-    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
-    <script src="{{ asset('js/vue.js') }}"></script>
+        </div> --}}
+    {{-- <script src="{{ asset('js/vue.js') }}"></script> --}}
     <script>
         let bus = new Vue()
         let Modal = {
